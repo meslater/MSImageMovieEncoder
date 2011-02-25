313b7fc32ca692ed9d6dac8dd8d656cf24d431cb
Michael Slater <m.e.slater@gmail.com>
2011-02-25 17:39:52 +0000

    Source and Readme Uploaded

---
+++ MSImageMovieEncoder.h
@@ -0,0 +1,126 @@
+//
+//  MSImageMovieEncoder.h
+//
+//  Created by Michael Slater (www.michaelslater.net) on 17/02/2011.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreMedia/CoreMedia.h>
+
+/** This protocol has 3 methods you must implement and a choice of 2 more, you must implement 1 of these optional methods for the class to function. */
+@protocol MSImageMovieEncoderFrameProvider <NSObject>
+
+//Information methods should all be implemented so you know what is going on.
+-(void)movieEncoderDidFailWithReason:(NSString*)reason;
+-(void)movieEncoderDidFinishAddingFrames;
+-(void)movieEncoderDidFinishEncoding;
+
+@optional
+//Just implement one of these methods - we check when we start requesting frames which one you've implemented
+//yes this adds a few instructions to requesting each frame but in the grand scheme of what's going on it's insignificant
+//and very convenient...
+
+/** Provides the delegate with a pointer to the CVPixelBuffer into which the frame should be drawn.
+ 
+ I recommend using -(BOOL)nextFrameInBGRCGBitmapContext:(CGContextRef*)contextRef if you want a bitmap context */
+-(BOOL)nextFrameInCVPixelBuffer:(CVPixelBufferRef*)pixelBuf;
+
+/** If you are going to be generating bitmap contexts anyway I recommend using this.
+ 
+ The only real optimisation is that it doesn't generate a colorSpace every frame, it caches it.  It just means you don't have to think about memory. 
+ */
+-(BOOL)nextFrameInBGRCGBitmapContext:(CGContextRef*)contextRef;
+
+@end
+
+typedef enum {
+	kMSMovieEncoderBGRCGBitmapContextModeMode,
+	kMSMovieEncoderCVPixelBufferMode
+} MovieEncoderMode;
+
+
+/** This class provides a simple way of generating a movie from a series of 'images' (CVPixelBufferRefs or CGContextRefs).
+ 
+ Simply implement the 3 status methods and 1 (ONE and only ONE) of the optional frame provider methods.
+ 
+ Init a new instance of this class, set the delegate to wherever you've implemented the apropriate delegate methods and call -(void)startRequestingFrames.
+ 
+ Your frame provider method will be handed a pointer to either a CVPixelBufferRef or a CGContextRef (depending on which you've implemented).  Fill the bitmap or buffer and return a BOOL to say if you've filled it.
+ 
+ As soon as you return NO the the movie encoding is finished and written to disk.
+ 
+ A simple frame provider class might look like this: (note that if you are providing frames from the class you declared the movie encoder in you can get the frame size directly from the encoder when you need it.)
+ 
+ CGSize frameSize;
+ int counter;
+ 
+ -(id)init {
+	 if (self = [super init]) {
+		 counter = 0;
+		 frameSize = CGSizeMake(1280, 720);
+	 }
+	 return self;
+ }
+ 
+ -(BOOL)drawNextAnimationFrameInContext:(CGContextRef *)contextRef {
+	 counter++;
+	 if (counter == 100) {
+		 return NO;
+	 }
+	 CGContextRef context = *contextRef;
+	 
+	 CGContextSetRGBFillColor(context, 0,0,1,1); //RED
+	 CGContextFillRect(context, CGRectMake(0, 0, frameSize.width, frameSize.height));
+	 
+	 CGContextSetRGBFillColor(context, 1,0,0,0.5); //BLUE with alpha of 0.5
+	 CGContextFillEllipseInRect(context, CGRectMake(1*counter, 200, 1*counter, 1*counter));
+	 return YES;
+ }
+ 
+ -(void)movieEncoderDidFinishAddingFrames {
+	//all that's left is the remaining compression (very quick)
+	NSLog(@"All frames added");
+ }
+ 
+ -(void)movieEncoderDidFinishEncoding {
+	//it's actually finished now, you can release it, if you like you can ask for the fileURL first so you know where the movie went
+	NSLog(@"Movie written to disk");
+ }
+ 
+ -(void)movieEncoderDidFailWithReason:(NSString *)reason {
+	//it's failed.  It's in an unpredictable state, the movie may exist but it's probably garbage if it does.
+	NSLog(@"%@", reason);
+ }
+ 
+ 
+ This class offers insanely good performance (through trial and error).  The key is that the assetWriter is writing in BGRA format.  Encoding 500 720p H264 frames in RGBA takes about 3 minutes.
+ Encoding the same in BGRA takes under 30 seconds.  Why?  I have no idea, presumably the HW encoder is BGRA and everything else has to be manually rotated in the CPU (weird).
+
+ Note that to set a colour in this context you should use:
+
+ CGContextSetRGBFillColor(context, BLUE,GREEN,RED,ALPHA); (The colours are in reverse order, the alpha is in the same place)
+
+ ie. CGContextSetRGBFillColor(context, 0,0,1.0,0.5); is 100% red with a 50% alpha.
+
+ For this slight annoyance of backwards colour declaration you get a 7x performance increase. */
+@interface MSImageMovieEncoder : NSObject {
+	CMTime frameDuration; /**< The duration of each frame (constant) */
+	CGSize frameSize;
+	NSURL* fileURL;
+	id<MSImageMovieEncoderFrameProvider> frameDelegate;
+}
+
+/** fURL MUST be a fileURL, fSize is the video frame size and the size of teh buffers you will be handed (ie CGSizeMake(1280,720), fDuration is the length of each frame */
++(id)pixelBufferMovieEncoderWithURL:(NSURL*)fURL andFrameSize:(CGSize)fSize andFrameDuration:(CMTime)fDuration;
+-(id)initWithURL:(NSURL*)fURL andFrameSize:(CGSize)fSize andFrameDuration:(CMTime)fDuration;
+
+
+//call this after you have set the delgate.  Calling while the delegate is nil will have no effect
+-(void)startRequestingFrames;
+
+@property (readwrite, nonatomic) CGSize frameSize; /**< The size of the video frame (ie. 1280x720) */
+@property (readonly) CMTime frameDuration; /**< The required duration of each still image.  ie. CMTimeMake(1, 25) would be 1/25th of a second (PAL) */
+@property (readonly) NSURL* fileURL; /**< The fileURL of the video, readonly as it cannot me changed after the writer is initialised. */
+@property (assign) id<MSImageMovieEncoderFrameProvider> frameDelegate; /**< The delegate providing frames */
+
+@end
---
+++ MSImageMovieEncoder.m
@@ -0,0 +1,241 @@
+//
+//  MSImageMovieEncoder.m
+//
+//  Created by Michael Slater (www.michaelslater.net) on 17/02/2011.
+//
+
+#import "MSImageMovieEncoder.h"
+#import <QuartzCore/QuartzCore.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface MSImageMovieEncoder (Private) //methods and variables only needed internally
+CMTime currentTime; //the current frame timing
+AVAssetWriter* assetWriter;
+AVAssetWriterInput* assetWriterInput;
+AVAssetWriterInputPixelBufferAdaptor* pixelBufferAdaptor;
+MovieEncoderMode mode;
+CGColorSpaceRef rgbColorSpace; //if we're making bitmapContexts keep this for the duration
+
+-(void)initialiseWriterWithURL:(NSURL*)videoLocation;
+-(CVPixelBufferRef)requestFrameFromDelegate;
+-(void)encodeAndWriteToDisk;
+@end
+
+@implementation MSImageMovieEncoder
+
+@synthesize frameSize, frameDuration, frameDelegate, fileURL;
+
+
+/** Initialise an auto-released movie encoder */
++(id)pixelBufferMovieEncoderWithURL:(NSURL *)fURL andFrameSize:(CGSize)fSize andFrameDuration:(CMTime)fDuration {
+	MSImageMovieEncoder* movieEncoder = [[MSImageMovieEncoder alloc] initWithURL:fURL andFrameSize:fSize andFrameDuration:fDuration];
+	return [movieEncoder autorelease];
+}
+
+-(id)initWithURL:(NSURL *)fURL andFrameSize:(CGSize)fSize andFrameDuration:(CMTime)fDuration {
+	if ([super init] == self) {
+		self.frameSize = fSize;
+		frameDuration = fDuration;
+		fileURL = fURL;
+		[self initialiseWriterWithURL:self.fileURL];
+	}
+	return self;
+}
+
+-(void)initialiseWriterWithURL:(NSURL*)videoLocation {
+	NSError *error = nil;
+	currentTime = kCMTimeZero; //set time to 0 when we begin
+	assetWriter = [[AVAssetWriter alloc] initWithURL:videoLocation
+											fileType:AVFileTypeAppleM4V
+											   error:&error];
+	
+	if(error) {
+		if ([self.frameDelegate respondsToSelector:@selector(movieEncoderDidFailWithReason:)]) {
+			[self.frameDelegate movieEncoderDidFailWithReason:[NSString stringWithFormat:@"Initialisation of movie encoder failed, assetWriter has error: %@", error]];
+		}
+    }
+	
+	NSDictionary *videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+								   AVVideoCodecH264, AVVideoCodecKey,
+								   [NSNumber numberWithFloat:self.frameSize.width], AVVideoWidthKey,
+								   [NSNumber numberWithInt:self.frameSize.height], AVVideoHeightKey,
+								   nil];
+	assetWriterInput = [[AVAssetWriterInput alloc] initWithMediaType:AVMediaTypeVideo outputSettings:videoSettings];
+	assetWriterInput.expectsMediaDataInRealTime = NO; //this is the default, here for clarity
+	
+	[assetWriter addInput:assetWriterInput];
+	
+	int local_bytesPerRow = self.frameSize.width * 4;
+	
+	NSDictionary *pixelBufferOptions = [[NSDictionary alloc] initWithObjectsAndKeys:
+										[NSNumber numberWithInt:kCVPixelFormatType_32BGRA], kCVPixelBufferPixelFormatTypeKey, //BGRA is SERIOUSLY BLOODY QUICK!
+										//kCFBooleanTrue, kCVPixelBufferCGImageCompatibilityKey,
+										kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey,
+										[NSNumber numberWithInt:local_bytesPerRow], kCVPixelBufferBytesPerRowAlignmentKey,
+										[NSNumber numberWithInt:self.frameSize.width], kCVPixelBufferWidthKey,
+										[NSNumber numberWithInt:self.frameSize.height], kCVPixelBufferHeightKey,
+										/* this doesn't work for the default allocator which is NULL itself */
+										//(CFAllocatorRef)kCFAllocatorSystemDefault, kCVPixelBufferMemoryAllocatorKey,
+										nil];
+	
+	pixelBufferAdaptor = [[AVAssetWriterInputPixelBufferAdaptor alloc] initWithAssetWriterInput:assetWriterInput sourcePixelBufferAttributes:pixelBufferOptions];
+	[pixelBufferOptions release];
+}
+
+/** This method starts the asset writer and sets the time to 0.  It then
+ creates a dispatch queue on which it executes a block which requests
+ frames from the frame delegate.  The delegate is passed a pointer to a CVPixelBuffer which it should
+ draw in.  When the delegate no longer has any frames available it should set the bufer to NULL.
+ This will cause the writer to finish writing, end the movie and begin compression.  Simple! */
+-(void)startRequestingFrames {
+	if (self.frameDelegate != nil) {
+		if ([self.frameDelegate respondsToSelector:@selector(nextFrameInBGRCGBitmapContext:)]) {
+			//prevents us from creating a colour space every time we produce a bitmap context.
+			rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+			mode = kMSMovieEncoderBGRCGBitmapContextModeMode;
+		}
+		else if ([self.frameDelegate respondsToSelector:@selector(nextFrameInCVPixelBuffer:)]) {
+			mode = kMSMovieEncoderCVPixelBufferMode;
+		}
+		//no use starting the whole thing up only to find the delegate doesn't exist.
+		//rather than checking before each frame (unnecessary), just check now that the delegate implements the appropriate method.
+	}
+	else {
+		return;
+	}
+
+	[assetWriter startWriting];
+	[assetWriter startSessionAtSourceTime:kCMTimeZero];
+
+	dispatch_queue_t queue = dispatch_queue_create("mscvpixelbuffer.framerequestqueue", NULL);
+	[assetWriterInput requestMediaDataWhenReadyOnQueue:queue usingBlock:^{
+        while ([assetWriterInput isReadyForMoreMediaData])
+        {
+			//this will always be a pixelbuffer regardless of what we actually got handed,
+			//requestFrameFromDelegate takes care of this for us.
+			CVPixelBufferRef nextPixelBuffer = [self requestFrameFromDelegate];
+			
+            if (nextPixelBuffer)
+            {
+                //if it can't be successfully appended let the delegate know and mark it as finished otherwise progress the time one frame
+				if (![pixelBufferAdaptor appendPixelBuffer:nextPixelBuffer withPresentationTime:currentTime]) {
+					if ([self.frameDelegate respondsToSelector:@selector(movieEncoderDidFailWithReason:)]) {
+						dispatch_async(dispatch_get_main_queue(), ^{ [self.frameDelegate movieEncoderDidFailWithReason:@"Bufer not appended successfully - Error compiling movie"]; });
+						
+					}
+					[assetWriterInput markAsFinished]; //since this one was unsuccessful we should bail... this prevents further calls from this block
+					[self encodeAndWriteToDisk];
+				}
+				else {
+					//rather than notifying the delegate after every frame the operation can be assumed successful if you don't get a fail notification.
+					currentTime = CMTimeAdd(currentTime, frameDuration); //progress frame to the end of the current frame
+				}
+				CVPixelBufferRelease(nextPixelBuffer);
+			}
+            else
+            {
+				if ([self.frameDelegate respondsToSelector:@selector(movieEncoderDidFinishAddingFrames)]) {
+					dispatch_async(dispatch_get_main_queue(), ^{ [self.frameDelegate movieEncoderDidFinishAddingFrames]; });
+				}
+				//now we've added all of the frames we need to finish encoding and writing to disk
+				[self encodeAndWriteToDisk]; //this is blocking because it has the blocking finishWriting call in it. (doesn't matter)
+                break;
+            }
+        }
+    }];
+	dispatch_release(queue);
+}
+
+-(void)encodeAndWriteToDisk {
+	if (rgbColorSpace) {
+		CGColorSpaceRelease(rgbColorSpace);
+		rgbColorSpace = NULL;
+	}
+	BOOL success = [assetWriter finishWriting];
+	if (!success) {
+		if ([self.frameDelegate respondsToSelector:@selector(movieEncoderDidFailWithReason:)]) {
+			dispatch_async(dispatch_get_main_queue(), ^{ [self.frameDelegate movieEncoderDidFailWithReason:[NSString stringWithFormat:@"Asset Writer could not write asset, returned with error: \n%@", [assetWriter error]]]; });
+			
+		}
+	}
+	else {
+		if ([self.frameDelegate respondsToSelector:@selector(movieEncoderDidFinishEncoding)]) {
+			dispatch_async(dispatch_get_main_queue(), ^{ [self.frameDelegate movieEncoderDidFinishEncoding]; });
+			
+		}
+	}
+
+}
+
+/** This method efficiently allocates a pixel buffer and requests that the delegate fill it.  It can also produce
+ a CGBitmapContext for you to save you some code / having to think about memory.
+ 
+ Because these buffers are being efficiently reused you MUST draw the entire frame (every pixel)
+ each time to avoid unexpected and unpredictable bits of other frames appearing.  The buffers are not reallocted
+ they are generally just reused and they are not wiped.  */
+-(CVPixelBufferRef)requestFrameFromDelegate {
+	//here we simply return a CVPixelBuffer and it is stuck to the end of the asset using the adaptor...
+	//For maximum efficiency, you should create CVPixelBuffer objects for appendPixelBuffer:withPresentationTime: by using this pool with the CVPixelBufferPoolCreatePixelBuffer function.
+	
+	CVPixelBufferRef pixelBuffer = NULL;
+	OSStatus err = CVPixelBufferPoolCreatePixelBuffer (kCFAllocatorDefault, pixelBufferAdaptor.pixelBufferPool, &pixelBuffer);
+	if (err) {
+		if ([self.frameDelegate respondsToSelector:@selector(movieEncoderDidFailWithReason:)]) {
+			dispatch_async(dispatch_get_main_queue(), ^{ [self.frameDelegate movieEncoderDidFailWithReason:[NSString stringWithFormat:@"CVPixelBufferPoolCreatePixelBuffer() failed with error %i", err]]; });
+			
+		}
+	}
+	
+	BOOL success = NO;
+	switch (mode) {
+		case kMSMovieEncoderBGRCGBitmapContextModeMode: //make a bitmap context and get the delegate to draw in it
+			if (pixelBuffer == NULL) {
+				break;
+			}
+			
+			CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+			void *pxdata = CVPixelBufferGetBaseAddress(pixelBuffer);
+			if (pxdata == NULL) {
+				return NO;
+			}
+			
+			CGContextRef context = CGBitmapContextCreate(pxdata, frameSize.width,
+														 frameSize.height, 8, 4*frameSize.width, rgbColorSpace,
+														 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+			success = [self.frameDelegate nextFrameInBGRCGBitmapContext:&context];
+
+			CGContextRelease(context);
+			
+			CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+			break;
+		case kMSMovieEncoderCVPixelBufferMode: //just pass a reference to the pixel buffer for the delegate to do with it what it likes
+			success = [self.frameDelegate nextFrameInCVPixelBuffer:&pixelBuffer];
+			break;
+		default:
+			break;
+	}
+	
+	if (!success) {
+		//the buffer won't be freed later (because it will never be appended) so do it here.
+		CVPixelBufferRelease(pixelBuffer);
+		pixelBuffer = NULL;
+	}
+	
+	return pixelBuffer;
+}
+
+
+
+
+
+-(void)dealloc {
+	CGColorSpaceRelease(rgbColorSpace);
+	[assetWriter release];
+	[assetWriterInput release];
+	[pixelBufferAdaptor release];
+	[fileURL release];
+	[super dealloc];
+}
+
+@end
